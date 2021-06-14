0bcaaabd73ab055a290776d23db64a1e8c60be46
sifan <msjabeer@accelaero.com>
2021-06-14 11:51:53 +0530

    kafka-producer

---
+++ src/main/java/guru/learningjournal/kafka/examples/AppConfigs.java
@@ -0,0 +1,8 @@
+package guru.learningjournal.kafka.examples;
+
+class AppConfigs {
+    final static String APPLICATIONID = "StorageDemo";
+    final static String BOOTSTRAPSERVERS = "localhost:9092";
+    final static String TOPICNAME = "invoice";
+    final static int NUMEVENTS = 500000;
+}
